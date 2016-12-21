d82730f261dd796e1daf4ccc053675446eac9bf6
tarekgabarin <tarekgabarin1992@gmail.com>
2016-12-21 17:09:44 -0500

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=e229160248a845a2fc1c832645bd25ee&units=imperial';
+
+
+module.exports = {
+  getTemp: function(location){
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      }
+      else {
+        return res.data.main.temp;
+
+      }
+    }, function(res) {
+      throw new Error(res.data.message);
+    });
+  }
+
+}
---
+++ app/components/About.jsx
@@ -0,0 +1,17 @@
+var About = require('About');
+var React = require('react');
+
+var About = React.createClass({
+
+  render: function () {
+    return (
+
+      <h3> About Component</h3>
+    );
+
+  }
+
+});
+
+
+module.exports = About;
