4d9a76cf306c9b29252304d517343a2a6be58fc4
tarekgabarin <tarekgabarin1992@gmail.com>
2016-12-22 11:28:53 -0500

    changes commited

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,8 +5,25 @@ var About = React.createClass({
 
   render: function () {
     return (
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>This is a weather application built on React.</p>
+        <p>
+          Here are some of the tools I used
+        </p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a>
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a>
 
-      <h3> About Component</h3>
+          </li>
+
+        </ul>
+
+      </div>
+
     );
 
   }
